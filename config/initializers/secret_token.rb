ed04d4d8d1a1657012177d4c7f131b9ba2bf3ff2
Dario Vallejos <dariov21@gmail.com>
2013-06-12 17:45:31 -0300

    Init structure of rails aplication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tecketee::Application.config.secret_token = '9c912ace34c17a4e5cdbb19009507e8bd086aa015b4be643b436abcf0ef517ac81a8c224912a4c4f418485265d3ce63be5dadc49b0af9f27ad6cbb3ab622e538'
